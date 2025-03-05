e256dd6204d0d1e1ba8df581f3e3d873f5c75df0
Wamiq <wamiqzahid319@gmail.com>
2025-03-05 14:28:42 +0500

    refactor: update Vite configuration and simplify HTML structure

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
-export default defineConfig({
+export default {
   server: {
-    proxy: {
-      "/api": "http://localhost:5000",
-    },
+    allowedHosts: [
+      "elementary-arab-event-individually.trycloudflare.com", // Add this host
+    ],
   },
-  plugins: [react()],
-});
+};
